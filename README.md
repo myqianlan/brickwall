12810247a7c353cdd0f38c612858b8fa04a71646
myqianlan <linquantan@gmail.com>
2015-02-05 16:08:53 +0800

    修正每行几像素的误差，增加自定义参数设置

--- README.md
+++ README.md
@@ -1,7 +1,27 @@
 BrickWall
 ===
 
-demo: 
 
 类似360，百度等图片搜索结果页面的效果的布局插件；正在开发中
 
+##如何使用
+
+```javascript
+    $("#demo").brickwall();
+```
+
+##API
+
+- brickMargin;
+    
+    图片外边距；
+    ```javascript
+    $("#demo").brickwall({brickMargin: 10});
+    ```
+
+- baseRowHeight;
+
+    基准高度（在此基础上做调整）；
+    ```javascript
+    $("#demo").brickwall({baseRowHeight: 150});
+    ```
--- js/brickwall.js
+++ js/brickwall.js
@@ -1,7 +1,7 @@
 /**
  *
  * @authors myqianlan (linquantan@gmail.com)
- * @date    2015年2月4日16:57:53
+ * @date    2015年2月5日16:02:10
  * @version $Id$
  */
 /*
@@ -10,9 +10,7 @@
  * 3. 获取容器宽度，设定一个子容器，依次叠加数据，如果宽大于容器宽度，保存。继续下一轮
  * 4. 获取每一个子容器里面内容的宽度，按照父容器宽度比例出高度。然后再次调整每个img的宽度数据；
  * 5. 将数据应用到图片
- * 6. 渲染到页面
  */
-
 
 ;
 (function($, window, document, undefined) {
@@ -20,11 +18,9 @@
     var BrickWall = function(ele, opt) {
         this.$el = ele;
         this.defaults = {
-            brickMargin: 10,
-            rowBaseHeight: 250,
-
+            brickMargin: 5,
+            rowBaseHeight: 150
         };
-        console.info(opt);
         this.options = $.extend({}, this.defaults, opt);
         this.imgData = {
             width: [],
@@ -34,21 +30,14 @@
     }
     BrickWall.prototype = {
         init: function() {
-
-            console.info("--------------------init--------------------------");
-            console.log("Hello Brick Wall");
             this._getImgInfo(this.$el.find("img"));
-            this._adjustImgFirst(this.imgData);
+            this._adjustImgFirst();
             this._addToRow();
             this._adjustImgSecond();
             this._applyDataToImg();
-            this._render();
-            console.info("////////////////////////DATA////////////////////////////");
-            console.info(this.imgData);
         },
         _getImgInfo: function($element) {
             var that = this;
-            console.info("--------------------getImgInfo--------------------------");
             // 1. 获取图片数据，保存图片原始宽高
             $.each($element, function(index, val) {
                 that.imgData.width[index] = $(val).width();
@@ -56,85 +45,81 @@
             });
 
         },
-        _adjustImgFirst: function(data) {
-            console.info("--------------------adjustImgFirst--------------------------");
+        _adjustImgFirst: function() {
             var that = this;
             // 2. 将保存的数据中的高统一为一个定值，获得其相应的宽
-            $.each(data.height, function(index, val) {
+            $.each(that.imgData.height, function(index, val) {
 
-                data.width[index] = Math.floor(data.width[index] * (that.options.rowBaseHeight / data.height[index]));
-                data.height[index] = that.options.rowBaseHeight;
+                that.imgData.width[index] = Math.floor(that.imgData.width[index] * (that.options.rowBaseHeight / that.imgData.height[index]));
+                that.imgData.height[index] = that.options.rowBaseHeight;
             });
         },
         _addToRow: function() {
             // 3. 获取容器宽度，设定一个子容器，依次叠加数据，如果宽大于容器宽度，保存。继续下一轮
-            console.info("--------------------addToRow--------------------------");
             var that = this;
             var rowImgWidth = 0;
             var rowImgArr = [];
-
+            var margin = that.options.brickMargin * 2;
             for (var i = 0, length = that.imgData.width.length; i < length; i++) {
 
-                if ((rowImgWidth + that.imgData.width[i]) <= that.$el.width()) {
-                    rowImgWidth = rowImgWidth + that.imgData.width[i];
+                if ((rowImgWidth + that.imgData.width[i] + margin) <= that.$el.width()) {
+                    rowImgWidth += that.imgData.width[i] + margin;
                     rowImgArr.push(i);
 
                 } else {
                     this.rowDivs.push(rowImgArr);
                     rowImgArr = [];
-                    rowImgWidth = that.imgData.width[i];
+                    rowImgWidth = that.imgData.width[i] + margin;
                     rowImgArr.push(i);
                 };
             };
             this.rowDivs.push(rowImgArr);
-            console.info(that.$el.width());
-            console.info(that.rowDivs);
         },
         _adjustImgSecond: function() {
             var that = this;
+            var margin = that.options.brickMargin * 2;
             // 4. 获取每一个子容器里面内容的宽度，按照父容器宽度比例出高度。然后再次调整每个img的宽度数据；
-            console.info("--------------------adjustImgSecond--------------------------");
             $.each(that.rowDivs, function(index, val) {
-                /* iterate through array or object */
-                console.info(val);
                 var totle = 0;
                 for (var i = val.length - 1; i >= 0; i--) {
-                    totle = totle + that.imgData.width[val[i]];
+                    totle += that.imgData.width[val[i]];
 
                 };
                 for (var j = val.length - 1; j >= 0; j--) {
-                    that.imgData.height[val[j]] = Math.floor(that.$el.width() / totle * that.imgData.height[val[j]]);
-                    that.imgData.width[val[j]] = Math.floor(that.$el.width() / totle * that.imgData.width[val[j]]);
+                    that.imgData.height[val[j]] = Math.floor((that.$el.width() - margin * val.length) / totle * that.imgData.height[val[j]]);
+                    that.imgData.width[val[j]] = Math.floor((that.$el.width() - margin * val.length) / totle * that.imgData.width[val[j]]);
                 };
+                var totleEnd = 0;
+                for (var m = val.length - 1; m >= 0; m--) {
+                    totleEnd += that.imgData.width[val[m]];
+
+                };
+                that.imgData.width[val[val.length - 1]] += (that.$el.width() - margin * val.length) - totleEnd;
             });
         },
         _applyDataToImg: function() {
             var that = this;
             // 5.将数据应用到图片
-            console.info("--------------------applyDataToImg--------------------------");
+            that.$el.find(".brick-item").css({
+                "float": "left",
+                "margin": that.options.brickMargin + "px"
+            });
+            that.$el.append("<div style='clear:both;'></div>");
             $.each(this.$el.find("img"), function(index, val) {
-                /* iterate through array or object */
-                console.info(val);
                 $(val).css({
                     width: that.imgData.width[index],
                     height: that.imgData.height[index]
                 })
             });
         },
-        _render: function() {
-            console.info("--------------------render--------------------------");
-                this.$el.find(".brick-item").css("float", "left");
-                this.$el.append("<div style='clear:both;'></div>");
-  
-        },
         _update: function(opt) {
             this.options = $.extend({}, this.defaults, opt);
+            console.info("message");
         }
     }
 
     $.fn.brickwall = function(options) {
         var brickwall = new BrickWall(this, options || {});
-        //调用其方法
         return brickwall.init();
     }
 })(jQuery, window, document);
